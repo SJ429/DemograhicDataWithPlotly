85fb0dd3ffde152a233b4e609009a84bb53d4760
Susan <sjessopb89@gmail.com>
2020-08-26 19:00:41 -0400

    updates

--- static/js/app.js
+++ static/js/app.js
@@ -1,60 +1,104 @@
+//Fetch the JSON data and console log it
+var sampleData = d3.json("data/samples.json")
 
-function SeeData(jsonData) {
-    var data = `data/samples.json`;
-   
-// // // Fetch the JSON data and console log it
- d3.json(data).then(function(jsonData) {
-      var metaJson = data.metaJson
-        console.log(metaJson);
-
-    var filteredmetaData = metaJson.filter(metadata => metadata == jsonData)[0];
-
+function createMeta(id) {
+    // Create metadata
+    d3.json(sampleData).then((data) => {
+      var metadata_array = data.metadata;
+      console.log(metadata_array);
+      console.log(id);
+      // Filter the metadata and build the demo panel using id
+      var metadataFiltered = metadata_array.filter(metadata => metadata.id == id)[0];
     //     Clear existing metadata
     d3.select("#sample-metadata").html("");
-
-    //  Use `Object.entries` to add dictionary of data and appendr 
-    Object.entries(filteredmetaData).forEach(([key, value]) => {
+    // Append metadata 
+        Object.entries(metadataFiltered).forEach(([key, value]) => {
       d3.select("#sample-metadata")
-        //append a paragraph tag
-        .append("p").text(`${key}: ${value}`);
-
+         .append("p").text(`${key}: ${value}`);
     });
   })
 }
-  
 function createPlots(id) {
-// //   // Create plots
- d3.json("samples.json").then((data) => {
-var samples = data.samples;
-//    // filter the samples to build the plots
-var filteredPlots=samples.filter(char=>char.id ==id)[0];
-var otu_ids = filteredPlots.otu_ids[0]
-var lables = filteredPlots.lables[0]
-var values = filteredPlots.values[0]
-//     //create plots
+    // Create plots
+    d3.json(sampleData).then((data) => {
+      var samples = data.samples;
+      console.log(samples)
+      // filter the samples to make plots
+      var plotsdataFiltered = samples.filter(chart => chart.id == id)[0]; 
+        //  console.log(plotsdataFiltered)
+      var otuIds = plotsdataFiltered.otuIds;
+      var sampleValues = plotsdataFiltered.sampleValues;
+      var otuLabels = plotsdataFiltered.otuLabels;
+ 
+    //Get the top ten data for each category with slice()
+    // top10 for sample_values,and otu_ids, and labels 
+      var otuIds = otu_ids.slice(0, 10);
+      var sampleValues = sample_values.slice(0, 10);
+      var otuLabels = otu_labels.slice(0, 10);
 
+    //Create the Bar Chart 
+      var trace = {
+      type: "bar",
+      x: sampleValuesTop10.reverse(),
+      y: otuIdsTop10.map(otu_ids => `OTU ${otu_ids}`).reverse(),
+      text: otuLabelsTop10.reverse(),
+      marker: {color: "blue",
+      },
+        orientation: 'h'
+    };
+      Plotly.newPlot("bar", trace, layout);
 
-//Create the Bar Plot 
-  var trace = {
-  type: "bar",
-  x: values
-  y: otu_ids.map(otu_ids => `OTU ${otu_ids}`)
-  
-  orientation: "h"
-  }
-  }
-}
-// // function optionChanged(id) {
-// //   createPlots(id);
-// //   createMeta(id);
-// // }
-// // function init() {
-// //   // create the dropdown menu
-// //   d3.json("samples.json").then((data) => {
-// //     console.log(data.names);
-// //   });
-// //   // Create metadata and build plots for first sample
-// //   createMeta("940");
-// //   // createPlots("940");
-// // }
-// // init();
+     // Create the Bubble Chart 
+       var trace1 = {
+        x: otuIds,
+        y: sampleValues,
+        mode: 'markers',
+        text: otuLabels,
+        marker: {
+          color:otuIds,
+          size: sampleValues,
+          colorscale: "Earth"
+        }
+      };
+    var trace1 = [{trace}];
+    var layout1 = {
+      title: "OTU ID",
+      showlegend: false,
+      height: 600,
+      width: 1500
+    };
+        Plotly.newPlot("bubble", trace1, layout1);
+    
+    // Build a Pie Chart
+      var trace2 = [{
+      values: sampleValues.slice(0, 10),
+      labels: otuIds.slice(0, 10),
+      hovertext: otuLabels.slice(0, 10),
+      type: "pie",
+      marker: {
+        colorscale: "Earth"
+      }
+    }];
+    var layout2 = {
+      showlegend: true,
+      height: 400,
+      width: 500
+    };
+     Plotly.newPlot("pie", trace2, layout2);
+
+function init(){
+  sampleData.then(function(data) {
+    sampleNames = data.names
+    // console.log(sampleNames)
+    selOptions = d3.select("#selDataset");
+    sampleNames.forEach(sampleid => {
+      selOptions.append("option")
+      .text(sampleid)
+      .property("value", sampleid);
+    })
+})
+
+} 
+
+init();
+
