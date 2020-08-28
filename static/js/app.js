5d4d8846f9c6044c096a675d876149af384386e1
Susan <sjessopb89@gmail.com>
2020-08-28 11:59:46 -0400

    updates

--- static/js/app.js
+++ static/js/app.js
@@ -31,15 +31,15 @@ function createPlots(id) {
           var otu_ids = plotsdataFiltered.otu_ids;
           var sample_values = plotsdataFiltered.sample_values;
           var otu_labels = plotsdataFiltered.otu_labels;
-          
+         
         //Get the top ten data for each category with slice() and reverse ids and 
         // sample_values.  
         //Getting the top 10 
           var otu_idstop = otu_ids.slice(0, 10).reverse();
           var sample_valuestop = sample_values.slice(0, 10).reverse();
           var otu_labelstop = otu_labels.slice(0, 10).reverse();
-        
-        //Create the Bar Chart 
+           
+          //Create the Bar Chart 
           var trace = {
                 type: "bar",
                 x: sample_valuestop,
@@ -78,7 +78,7 @@ function createPlots(id) {
               };
             Plotly.newPlot("bubble", data1, layout1);
         
-        // Build a Pie Chart
+        // Create a Pie Chart
           var trace2 = {
                 values: sample_valuestop,
                 labels: otu_idstop,
@@ -94,7 +94,44 @@ function createPlots(id) {
               });    
       
             };
-      
+            // Create a Gauge Chart
+          function gauge(id) {
+          var sampleData = ("data/samples.json")
+          d3.json("data/samples.json").then(function(data){
+              console.log(data)
+              var plotsdataFiltered = plotsData.filter(chart => chart.id.toString( ) === id)[0]; 
+              console.log(plotsdataFiltered)
+              // var wfreq =plotsdataFiltered.wfreq
+              // var wfreq = data.metadata.map(data => data.wfreq)
+              //  console.log(`Washing Freq: ${wfreq}`)
+
+              var data3 = [
+                {     
+                    domain: { x: [0, 1], y: [0, 1] },
+                    value: wfreq,
+                    title: { text: "Weekly Washing Frequency" },
+                    type: "indicator",
+                    mode: "gauge+number+delta",
+                    delta: { reference: 380 },
+                    gauge: {axis: { range: [null, 500] },
+                            steps: [
+                        { range: [0, 250], color: "lightgray" },
+                        { range: [250, 400], color: "gray" }
+                        ],
+                      threshold: {
+                        line: { color: "teal", width: 4 },
+                        thickness: 0.75,
+                        value: wfreq
+                      } 
+                    }
+                  }     
+              ];    
+              var layout3 = { width: 600, height: 450, margin: { t: 0, b: 0 } };
+             Plotly.newPlot('gauge', data3, layout3);
+     
+ 
+    })
+  }
 //Get the functions to display the data and the plots to the page
 function optionChanged(id) {
               createPlots(id)[0];
@@ -110,12 +147,12 @@ function init(){
     // console.log(sampleNames)
           sampleNames.forEach(sampleNames=> {selOptions.append("option")
             .text(sampleNames)
-            .property("value", sampleNames);
+            .property("value");
           })
     // Use the first sample from the list to build the initial plots on page
         const showPlots = sampleNames[0];
         createPlots(showPlots);
-        getMetaData(showPlots)
+        
       })
 
     
